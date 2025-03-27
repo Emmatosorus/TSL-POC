e8d456eb7b1a2389ce8a7f61d5eda038d8c2650c
Emmatosorus <emma@bouteyres.com>
2025-03-27 17:02:10 +0100

    Add ✨: Coffee Smoke POC with tsl

--- TSL-Coffee-Smoke/src/script.js
+++ TSL-Coffee-Smoke/src/script.js
@@ -3,17 +3,17 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import {Pane} from 'tweakpane'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import {
-    vec3,
-    sin,
-    uniform,
     vec4,
-    mix,
-    float,
     time,
-    range,
-    PI2,
-    cos,
     uv,
+    positionLocal,
+    texture,
+    Fn,
+    vec2,
+    smoothstep,
+    rotate,
+    mul,
+    pow
 } from "three/tsl";
 
 // Debug
@@ -81,6 +81,82 @@ gltfLoader.load(
         scene.add(gltf.scene)
     }
 )
+
+/**
+ * Smoke
+ */
+const smokeGeometry = new THREE.PlaneGeometry(1, 1, 16, 64)
+smokeGeometry.translate(0, 0.5, 0)
+smokeGeometry.scale(1.5, 6, 1.5)
+
+const smokeMaterial = new THREE.MeshBasicNodeMaterial({
+    side: THREE.DoubleSide,
+    transparent: true,
+    depthWrite: false
+})
+
+pane.addBinding(smokeMaterial, 'wireframe')
+
+const perlinTexure = textureLoader.load('./perlin.png')
+perlinTexure.wrapS = THREE.RepeatWrapping
+perlinTexure.wrapT = THREE.RepeatWrapping
+
+/**
+ * -------- Coffee Smoke in TSL --------
+ */
+
+/**
+ * Position Node
+ */
+smokeMaterial.positionNode = Fn(() => {
+
+    const newPosition = positionLocal
+
+    // Twist
+    const twistPerlin = texture(perlinTexure, vec2(0.5, uv().y.mul(0.1).sub(time.mul(0.002)))).r
+    const angle = twistPerlin.mul(10)
+    newPosition.xz.assign(rotate(newPosition.xz, angle))
+
+    // Wind
+    const windOffset = vec2(
+        texture(perlinTexure, vec2(0.25, time.mul(0.005))).r.sub(0.5),
+        texture(perlinTexure, vec2(0.75, time.mul(0.005))).r.sub(0.5)
+    )
+    windOffset.mulAssign(mul(5, pow(uv().y, 2)))
+    newPosition.xz.addAssign(windOffset)
+
+    return newPosition
+})()
+
+
+/**
+ * Fragment Node
+ */
+
+smokeMaterial.fragmentNode = Fn(() => {
+    // Scale and animate
+    const smokeUv = uv().toVar()
+    smokeUv.x.mulAssign(0.5)
+    smokeUv.y.mulAssign(0.3)
+    smokeUv.y.subAssign(time.mul(0.03))
+
+    const smoke = texture(perlinTexure, smokeUv).r
+
+    // Remap
+    smoke.assign(smoothstep(0.45, 1.0, smoke))
+
+    // Smooth edges
+    smoke.mulAssign(smoothstep(0.0, 0.1, uv().x))
+    smoke.mulAssign(smoothstep(1.0, 0.9, uv().x))
+    smoke.mulAssign(smoothstep(0.0, 0.2, uv().y))
+    smoke.mulAssign(smoothstep(1.0, 0.4, uv().y))
+
+    return vec4(0.6, 0.3, 0.2, smoke.mul(0.5))
+})()
+
+const smokeMesh = new THREE.Mesh(smokeGeometry, smokeMaterial)
+smokeMesh.position.y = 1.83
+scene.add(smokeMesh)
 
 /**
  * Animate
