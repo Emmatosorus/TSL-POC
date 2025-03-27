a7cf6b47a8044a94d1251cfa640b499469ce5b55
Emmatosorus <emma@bouteyres.com>
2025-03-27 15:18:23 +0100

    Add ✨: Coffee Smoke POC (Without TSL)

---
+++ TSL-Coffee-Smoke/src/script.js
@@ -0,0 +1,98 @@
+import * as THREE from 'three/webgpu'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {Pane} from 'tweakpane'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import {
+    vec3,
+    sin,
+    uniform,
+    vec4,
+    mix,
+    float,
+    time,
+    range,
+    PI2,
+    cos,
+    uv,
+} from "three/tsl";
+
+// Debug
+const pane = new Pane({title: 'Coffee Smoke ☕'})
+
+/**
+ * Base
+ */
+
+const canvas = document.getElementById('webgpu')
+const scene = new THREE.Scene()
+
+// Loaders
+const textureLoader = new THREE.TextureLoader()
+const gltfLoader = new GLTFLoader()
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 200)
+camera.position.set(8, 10, 12)
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.target.y = 3
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGPURenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Model
+ */
+gltfLoader.load(
+    './bakedModel.glb',
+    (gltf) =>
+    {
+        gltf.scene.getObjectByName('baked').material.map.anisotropy = 8
+        scene.add(gltf.scene)
+    }
+)
+
+/**
+ * Animate
+ */
+
+const tick = () =>
+{
+    controls.update()
+
+    renderer.renderAsync(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
