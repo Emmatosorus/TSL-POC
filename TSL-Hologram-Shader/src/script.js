074d6ae53273e8f099df06f7b94a159ea634750b
Emmatosorus <emma@bouteyres.com>
2025-03-28 13:56:56 +0100

    Add ✨: Hologram Shader with TSL

--- TSL-Hologram-Shader/src/script.js
+++ TSL-Hologram-Shader/src/script.js
@@ -5,19 +5,25 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import {
     vec4,
     time,
-    uv,
-    positionLocal,
-    texture,
     Fn,
     vec2,
     smoothstep,
-    rotate,
-    mul,
-    pow
+    pow,
+    positionWorld,
+    mod,
+    dot,
+    cameraPosition,
+    If,
+    uniform,
+    fract,
+    sin,
+    normalWorld,
+    positionLocal
 } from "three/tsl";
 
 // Debug
 const pane = new Pane({title: 'Hologram Shader 🌐'})
+const debugObject = {}
 
 /**
  * Base
@@ -79,12 +85,77 @@ pane.addBinding(rendererParameters, 'clearColor', {label: 'Clear Color'}).on('ch
 /**
  * Material
  */
-const material = new THREE.MeshBasicMaterial()
+const material = new THREE.MeshBasicNodeMaterial({
+    side: THREE.DoubleSide,
+    transparent: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending
+})
+
+pane.addBinding(material, 'wireframe')
+
+debugObject.holographicColor = '#70c1ff'
+const uColor = uniform(new THREE.Color(debugObject.holographicColor))
+pane.addBinding(debugObject, 'holographicColor', {label: 'Color'}).on('change', () => {
+    uColor.value = new THREE.Color(debugObject.holographicColor)
+})
+
+/**
+ * Vertex shader
+ */
+// Function that returns a "random" float in range [0.0, 1.0[
+const random2D = Fn(([value]) => {
+    return fract(sin(dot(value.xy, vec2(12.9898, 78.233))).mul(43758.5453123))
+})
+
+
+material.positionNode = Fn(() => {
+    // Position
+    const position = positionLocal.toVar()
+
+    // Glitch
+    const glitchTime = time.sub(positionWorld.y).toVar()
+    const glitchStrength = sin(glitchTime).add(sin(glitchTime.mul(3.45))).add(sin(glitchTime.mul(8.76))).div(3.0).toVar()
+    glitchStrength.assign(smoothstep(0.3, 1.0, glitchStrength).mul(0.25))
+    position.x.addAssign(random2D(position.xz.add(time)).sub(0.5).mul(glitchStrength))
+    position.z.addAssign(random2D(position.zx.add(time)).sub(0.5).mul(glitchStrength))
+
+    return position
+})()
+
+/**
+ * Fragment shader
+ */
+material.fragmentNode = Fn(() => {
+    // Normal
+    const normal = normalWorld
+    If(dot(normal, cameraPosition).lessThan(0.0), () => {
+        normal.mulAssign(-1.0)
+    })
+
+    // Stripes
+    const stripes = mod(positionWorld.y.sub(time.mul(0.02)).mul(20), 1.0).toVar()
+    stripes.assign(pow(stripes, 3.0))
+
+    // Fresnel
+    const viewDirection = positionWorld.sub(cameraPosition).normalize().toVar()
+    const fresnel = dot(viewDirection, normal).add(1.0).toVar()
+    fresnel.assign(pow(fresnel, 4.0))
+
+    // Falloff
+    const falloff = smoothstep(0.8, 0.0, fresnel).toVar()
+
+    // Holographic
+    const holographic = stripes.mul(fresnel).toVar()
+    holographic.addAssign(fresnel.mul(1.25))
+    holographic.mulAssign(falloff)
+
+    return vec4(uColor, holographic)
+})()
 
 /**
  * Objects
  */
-// Torus knot
 const torusKnot = new THREE.Mesh(
     new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
     material
